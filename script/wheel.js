d1251a6eb53456f2682871a4576981d4eb5315c3
chrisblock <chris.block@gmail.com>
2010-11-20 23:31:01 -0600

    Added configuration option for background and foreground colors.

--- script/wheel.js
+++ script/wheel.js
@@ -19,7 +19,9 @@
 		radius,
 		rotation = 0,
 		centerOfWheelX,
-		centerOfWheelY;
+		centerOfWheelY,
+		foreground,
+		background;
 	
 	var getDisplayString = function (item) {
 		var str = item;
@@ -52,10 +54,57 @@
 		}
 		return htmlItems;
 	};
+
+	var buildRgbObject = function (color) {
+		if(!!color.match(/#[\dA-F]{6}/i)) {
+			var redString = color.substring(1, 3);
+			var red = parseInt(redString, 16);
+
+			var greenString = color.substring(3, 5);
+			var green = parseInt(greenString, 16);
+
+			var blueString = color.substring(5, 7);
+			var blue = parseInt(blueString, 16);
+			return {
+				r: red,
+				g: green,
+				b: blue
+			};
+		}
+	};
+
+	var getColorShade = function (fg, bg, percent) {
+		return {
+			r: getColorComponentShade(fg.r, bg.r, percent),
+			b: getColorComponentShade(fg.b, bg.b, percent),
+			g: getColorComponentShade(fg.g, bg.g, percent) 
+		};
+	};
+
+	var padColor = function(color) {
+		var leadingZeroes = '00';
+		var colorString = color.toString(16);
+		return leadingZeroes.substring(0, leadingZeroes.length - colorString.length) + colorString;
+	};
+
+	var getColorString = function (rgb) {
+		var str = '#';
+		str += padColor(rgb.r);
+		str += padColor(rgb.g);
+		str += padColor(rgb.b);
+		return str;
+	};
+
+	var getColorComponentShade = function (fg, bg, p) {
+		var diff = (bg - fg) * p;
+		var shade = Math.floor(fg + diff);
+		return shade;
+	};
 	
-	var calculateShadeOfGrey = function (zIndex) {
-		var color = (0xFF - Math.floor(0x99 * (1 - zIndex / (2 * radius)))).toString(16);
-		return '#' + color + color + color;
+	var getBackgroundColor = function (zIndex) {
+		var percent = (1 - zIndex / (2 * radius));
+		var color = getColorShade(foreground, background, percent);
+		return getColorString(color);
 	};
 	
 	var draw = function () {
@@ -73,7 +122,7 @@
 			var x = centerOfWheelX - horizontalSkewOffset;
 			var y = centerOfWheelY + verticalSkewOffset - Math.floor(radius * sine(currentAngle));
 			var style = 'position:absolute;top:' + y + 'px;left:' + x + 'px;z-index:' + zIndex + ';';
-			var bgColor = calculateShadeOfGrey(zIndex);
+			var bgColor = getBackgroundColor(zIndex);
 			style += 'background-color:' + bgColor + ';';
 			var item = htmlItems[i];
 			item.attr('style', style);
@@ -128,6 +177,9 @@
 		pitch = config.pitch || 5;
 		yaw = config.yaw || 5;
 		setDisplayProperty(config.displayProperty || 'value');
+		
+                foreground = buildRgbObject(config.foreground || '#EEEEEE');
+                background = buildRgbObject(config.background || '#999999');
 		
 		setItems(config.items);
 		setFilters(config.filters || []);
